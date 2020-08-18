9ce07846f8dba5266d9b94719ebfda1e90b31af6
Scc_hy <34824890+scchy@users.noreply.github.com>
2020-08-19 01:48:55 +0800

    Create MNIST1_sklearn.ensemble集成模型训练.py

---
+++ MNIST1_sklearn.ensemble集成模型训练.py
@@ -0,0 +1,41 @@
+# python 3.6
+# Author: Scc_hy
+# Create date: 2020-08-19
+# Function: 用sklearn 包对 mnist识别
+
+import pandas as pd 
+import numpy as np
+from sklearn.datasets import fetch_mldata
+import warnings
+warnings.filterwarnings(action='ignore')
+
+def get_ministdata():
+    data_home = r'D:\Python_data\My_python\Projects\MNIST_Subject\mnist_data'
+    mnist = fetch_mldata('MNIST original', data_home=data_home)
+    return pd.DataFrame(np.c_[mnist['data']/255, mnist['target']])
+
+
+def sklearn_clf(clf_model_func, tr, te):
+    clf_model = clf_model_func()
+    clf_model.fit(tr.iloc[:, :-1].values, tr.iloc[:, -1].values)
+    pred = clf_model.predict(te.iloc[:, :-1].values)
+    y_te =  te.iloc[:, -1].values
+    acc_ = sum(pred == y_te)/len(y_te) * 100
+    return f'model: {clf_model_func.__name__}, acc: {acc_:.2f}'
+
+
+from sklearn.ensemble import RandomForestClassifier
+import sklearn as skl
+if __name__ == '__main__':
+    mnistdf = get_ministdata()
+    te_index = mnistdf.sample(frac=0.8).index.tolist()
+    mnist_te = mnistdf.loc[te_index, :]
+    mnist_tr = mnistdf.loc[~mnistdf.index.isin(te_index), :]
+    # 用集成模型训练 & 预测
+    ensemble_func_lst = [i for i in dir(skl.ensemble) if 'Classifier' in i and 'Voting' not in i]
+    print(ensemble_func_lst)
+    for clf_ in ensemble_func_lst:
+        print(f'test clf_: {clf_}')
+        msg=sklearn_clf(eval(f'skl.ensemble.{clf_}'), mnist_tr, mnist_te)
+        print(msg)
+
