fc9773bcbc28879fa61e310423f06149287a8d21
Scc_hy <34824890+scchy@users.noreply.github.com>
2020-08-20 02:29:37 +0800

    增加评测结果

--- MNIST2_LGB_XGB.py
+++ MNIST2_LGB_XGB.py
@@ -32,36 +32,39 @@ def get_ministdata():
 
 lgb_param = {
     'boosting': 'gbdt',
-    'num_iterations': 150,
+    'num_iterations': 145,
     'num_threads' : 8, 
     'verbosity': 0,
-    'learning_rate': 0.1,
-    'max_depth' : 8,
-    'num_leaves' : 10,
-    'subsample' : 0.8,
+    'learning_rate': 0.2,
+    'max_depth' : 10,
+    'num_leaves' : 8,
+    'subsample' : 0.75,
     'subsample_freq': 5,
     'colsample_bytree' : 1,
-    'reg_alpha': 0.8,
-    'reg_lambda': 0.3,
+    'reg_alpha': 1.5,
+    'reg_lambda': 0.75,
     'objective': 'multiclass',
     'num_class': 10,
     'metric': 'multi_logloss',
     'early_stopping': 25
+    # 'device': 'gpu',
+    # 'gpu_platform_id': 0,
+    # 'gpu_device_id': 0
 }
 
 xgb_param = {
     'booster': 'gbtree',
     'tree_method':'gpu_hist',
-    'num_rounds': 150,
+    'num_rounds': 160,
     'nthread' : 8, 
     'silent' : 1,
-    'learning_rate': 0.1,
-    'max_depth' : 8,
-    'num_leaves' : 10,
-    'subsample' : 0.8,
+    'learning_rate': 0.2,
+    'max_depth' : 10,
+    'num_leaves' : 8,
+    'subsample' : 0.75,
     'colsample_bytree' : 1,
-    'reg_alpha': 0.8,
-    'reg_lambda': 0.3,
+    'reg_alpha': 1.5,
+    'reg_lambda': 0.75,
     'objective': 'multi:softprob',
     'num_class': 10,
     'metric': 'mlogloss',
@@ -99,6 +102,29 @@ if __name__ == '__main__':
     print('train lgb ...')
     reslgb = lgb_xgb_train(lgb, lgb_param, mnist_tr, mnist_te)
 
+
 """
+train xgb ...
+[0]     train-merror:0.085857   test-merror:0.149357
+[9]     train-merror:0.014714   test-merror:0.067696
+lgb_xgb_train, take_time:34.88644s >> model: xgboost, acc: 93.23
+train lgb ...
+Training until validation scores don't improve for 25 rounds.
+[20]    training's multi_logloss: 0.375235      valid_1's multi_logloss: 0.420369
+[40]    training's multi_logloss: 0.188535      valid_1's multi_logloss: 0.256125
+[60]    training's multi_logloss: 0.116575      valid_1's multi_logloss: 0.202916
+[80]    training's multi_logloss: 0.0781876     valid_1's multi_logloss: 0.177008
+[100]   training's multi_logloss: 0.0550978     valid_1's multi_logloss: 0.161271
+[120]   training's multi_logloss: 0.0408921     valid_1's multi_logloss: 0.151961
+[140]   training's multi_logloss: 0.0318648     valid_1's multi_logloss: 0.146336
+[LightGBM] [Warning] No further splits with positive gain, best gain: -inf
+[LightGBM] [Warning] No further splits with positive gain, best gain: -inf
+[LightGBM] [Warning] No further splits with positive gain, best gain: -inf
+[LightGBM] [Warning] No further splits with positive gain, best gain: -inf
+[LightGBM] [Warning] No further splits with positive gain, best gain: -inf
+[LightGBM] [Warning] No further splits with positive gain, best gain: -inf
+Did not meet early stopping. Best iteration is:
+[145]   training's multi_logloss: 0.030263      valid_1's multi_logloss: 0.145314
+lgb_xgb_train, take_time:24.91101s >> model: lightgbm, acc: 95.65
 
 """
